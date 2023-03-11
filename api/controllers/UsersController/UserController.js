5a01460bf27422dfd72490a9198cc2ff2e1f934a
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-03-10 23:16:19 -0300

    feat(UserController): validation for password with repeat password

--- api/controllers/UsersController/UserController.js
+++ api/controllers/UsersController/UserController.js
@@ -1,64 +1,55 @@
 /* eslint-disable eqeqeq */
 const express = require('express');
 const router = express.Router();
-const Users = require('../../api/api/models/user/Users');
+const Users = require('../../models/user/Users');
 
 router.post('/:restaurantid/admin/users/save', (req, res) => {
-  const { name, lastName, dateBirth, permissions, password } = req.body;
+  const { name, lastName, dateBirth, permissions, password, repeatpassword } = req.body;
   const { restaurantId } = req.params;
-  const userName = GetUserName(name, dateBirth);
 
-  Users.findOne({ where: { userName } }).then((user) => {
-    if (user == undefined) {
-      CreateUser(
-        name,
-        lastName,
-        dateBirth,
-        permissions,
-        userName,
-        password,
-        restaurantId
-      );
-      res.status(200).json({ message: 'Cadastro realizado com sucesso!' });
+  if (restaurantId != undefined) {
+    const userName = GetUserName(name, dateBirth);
+    if (password === repeatpassword) {
+      Users.findOne({ where: { userName } }).then((user) => {
+        if (user == undefined) {
+          CreateUser(name, lastName, dateBirth, permissions, userName, password, restaurantId);
+          res.status(200).json({ message: 'Cadastro realizado com sucesso!' });
+        } else {
+          res.status(401).json({ message: 'Usuário ja cadastrado!' });
+        }
+      });
     } else {
-      res.status(401).json({ message: 'Usuário ja cadastrado!' });
+      res.status(403).json({ message: 'As senhas não conferem!' });
     }
-  });
+  } else {
+    res.status(400).json({ message: 'id não informado' });
+  }
 });
 
-router.patch('/:restaurantid/admin/user/:userid', (req, res) => {
-  const { name, lastName, dateBirth, permissions, password } = req.body;
+router.patch('/:restaurantId/admin/user/:userid', (req, res) => {
+  const { name, lastName, dateBirth, permissions, password, repeatpassword } = req.body;
   const { restaurantId, userid } = req.params;
-  const userName = GetUserName(name, dateBirth);
 
-  Users.findOne({ where: { id: userid } }).then((user) => {
-    if (user != undefined) {
-      UpdateUser(
-        userid,
-        name,
-        lastName,
-        dateBirth,
-        permissions,
-        userName,
-        password,
-        restaurantId
-      );
-      res.status(200).json({ message: 'Cadastro atualizado com sucesso!' });
+  if (restaurantId != undefined) {
+    const userName = GetUserName(name, dateBirth);
+    if (password === repeatpassword) {
+      Users.findOne({ where: { id: userid } }).then((user) => {
+        if (user != undefined) {
+          UpdateUser(userid, name, lastName, dateBirth, permissions, userName, password, restaurantId);
+          res.status(200).json({ message: 'Cadastro atualizado com sucesso!' });
+        } else {
+          res.status(401).json({ message: 'Usuário não encontrado' });
+        }
+      });
     } else {
-      res.status(401).json({ message: 'Usuário não encontrado' });
+      res.status(403).json({ message: 'As senhas não conferem!' });
     }
-  });
+  } else {
+    res.status(400).json({ message: 'id não informado' });
+  }
 });
 
-function CreateUser(
-  name,
-  lastName,
-  dateBirth,
-  permissions,
-  userName,
-  password,
-  restaurantId
-) {
+function CreateUser(name, lastName, dateBirth, permissions, userName, password, restaurantId) {
   Users.create({
     name,
     lastName,
@@ -70,16 +61,7 @@ function CreateUser(
   });
 }
 
-function UpdateUser(
-  userid,
-  name,
-  lastName,
-  dateBirth,
-  permissions,
-  userName,
-  password,
-  restaurantId
-) {
+function UpdateUser(userid, name, lastName, dateBirth, permissions, userName, password, restaurantId) {
   Users.update(
     {
       name,

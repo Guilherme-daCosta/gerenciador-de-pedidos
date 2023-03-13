701b706d25f68b389403f35a27d7ba90d8eed056
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-03-13 11:15:41 -0300

    refactor

--- api/models/database.js
+++ api/models/database.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const Sequelize = require('sequelize');
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASS;
 
-const connection = new Sequelize('order_manager', 'admin', '', {
-  host: 'localhost',
+const connection = new Sequelize('order_manager', user, password, {
+  host,
   dialect: 'mysql',
   timezone: '-03:00'
 });

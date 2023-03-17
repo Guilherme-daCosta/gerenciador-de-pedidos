91bff9bd8354865b711445beba3aa1e281a4bc08
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-03-17 20:22:15 -0300

    feat: route created for item update

--- api/controllers/AdditionalsController/AdditionalsController.js
+++ api/controllers/AdditionalsController/AdditionalsController.js
@@ -41,6 +41,28 @@ router.post('/:restaurantId/additional/save', CheckToken, async(req, res) => {
   }
 });
 
+router.patch('/:restaurantId/additional/:id/update', CheckToken, async(req, res) => {
+  const { item, price, categorie } = req.body;
+  const { id, restaurantId } = req.params;
+
+  const itemExists = Additional.findOne({ where: { id } });
+  if (!itemExists) {
+    return res.status(401).json({ message: 'O item não esta cadastrado!' });
+  }
+
+  try {
+    Additional.update({
+      item,
+      price,
+      categorie,
+      restaurantId
+    }, { where: { id } });
+    res.status(200).json({ message: 'Item adicionado com sucesso!' });
+  } catch (err) {
+    res.send(400).json({ message: err });
+  }
+});
+
 function CheckToken(req, res, next) {
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(' ')[1];

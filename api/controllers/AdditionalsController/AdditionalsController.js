ecb98386c196d84f09c0fd0cf440f7747d39f51e
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-03-17 19:50:58 -0300

    feat(AdditionalsController): added JWT

--- api/controllers/AdditionalsController/AdditionalsController.js
+++ api/controllers/AdditionalsController/AdditionalsController.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const Additional = require('../../models/additional/Additional');
+const jwt = require('jsonwebtoken');
 
-router.get('/additional', (req, res) => {
+router.get('/additional', CheckToken, (req, res) => {
   Additional.findall();
   console.log('teste tabela');
 });
+
+function CheckToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Acesso negado!' });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+
+    jwt.verify(token, secret);
+    next();
+  } catch (err) {
+    res.status(400).json({ message: 'Token inválido!' });
+  }
+}
 
 module.exports = router;

b361e23a474326df94d7964b4618352dc483f14f
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-03-20 20:56:24 -0300

    refactor

--- src/controllers/UsersController/UserController.js
+++ src/controllers/UsersController/UserController.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const Users = require('../../models/user/Users');
+const Users = require('../../../src/models/user/Users');
 const jwt = require('jsonwebtoken');
 
 router.get('/:restaurantid/admin/users', CheckToken, (req, res) => {

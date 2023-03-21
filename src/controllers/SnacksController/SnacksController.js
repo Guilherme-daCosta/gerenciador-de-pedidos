affd3f466820fbf065e58bf0a612fb817e38dc38
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-03-20 23:50:24 -0300

    feat: add CheckToken to snacks

--- src/controllers/SnacksController/SnacksController.js
+++ src/controllers/SnacksController/SnacksController.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Snack = require('../../models/Snacks');
+const CheckToken = require('../../Util/AuthJWT');
 
-router.get('/additional', (req, res) => {
+router.get('/additional', CheckToken, (req, res) => {
   Snack.findall();
   console.log('teste tabela');
 });

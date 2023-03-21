9c1a3b70461544596ab77783c05d39d44348f160
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-03-20 23:42:59 -0300

    feat: created route for update registration

---
+++ src/controllers/Restaurant/Update.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const Restaurant = require('../../models/restaurant/Restaurant');
+const CheckToken = require('../../Util/AuthJWT');
+
+router.patch('/:restaurantId/update', CheckToken, async(req, res) => {
+  const { name, cnpj, email } = req.body;
+  const { restaurantId } = req.params;
+
+  if (!name) {
+    res.status(404).json({ message: 'Nome é obrigatório' });
+  }
+
+  if (!cnpj) {
+    res.status(404).json({ message: 'CNPJ é obrigatório' });
+  }
+
+  if (!email) {
+    res.status(404).json({ message: 'Email é obrigatório' });
+  }
+
+  try {
+    Restaurant.update({
+      name,
+      cnpj,
+      email
+    },
+    { where: { id: restaurantId } });
+    res.status(200).json({ message: 'Cadastro atualizado com sucesso!' });
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
+});
+
+module.exports = router;

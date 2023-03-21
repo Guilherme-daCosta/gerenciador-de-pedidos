474186e612ce049473810188cf1a32c6fba6262a
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-03-20 21:39:23 -0300

    refator in route of delete users

---
+++ src/controllers/Users/Delete.js
@@ -0,0 +1,24 @@
+/* eslint-disable eqeqeq */
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const Users = require('../../../src/models/user/Users');
+const CheckToken = require('../../Util/AuthJWT');
+
+router.delete('/:restaurantId/admin/user/:userId', CheckToken, async(req, res) => {
+  const { userId } = req.params;
+  const userExists = await Users.findOne({ where: { id: userId } });
+
+  if (!userExists) {
+    return res.status(422).json({ message: 'O usuário não existe' });
+  }
+
+  try {
+    Users.destroy({ where: { id: userId } });
+    res.status(200).json({ message: 'Usuário deletado com sucesso!' });
+  } catch (err) {
+    res.send(400).json({ message: err });
+  }
+});
+
+module.exports = router;
--- src/controllers/UsersController/UserController.js
+++ src/controllers/UsersController/UserController.js
@@ -3,25 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const Users = require('../../../src/models/user/Users');
-const CheckToken = require('../../Util/AuthJWT');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-
-router.delete('/:restaurantId/admin/user/:userId', CheckToken, async(req, res) => {
-  const { userId } = req.params;
-  const userExists = await Users.findOne({ where: { id: userId } });
-
-  if (!userExists) {
-    return res.status(422).json({ message: 'O usuário não existe' });
-  }
-
-  try {
-    Users.destroy({ where: { id: userId } });
-    res.status(200).json({ message: 'Usuário deletado com sucesso!' });
-  } catch (err) {
-    res.send(400).json({ message: err });
-  }
-});
 
 router.post('/auth/user', async(req, res) => {
   const { userName, password } = req.body;

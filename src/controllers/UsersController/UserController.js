6595d5ec023a1d841140028db650e526d4a8750b
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-03-20 21:35:21 -0300

    refactor

--- src/controllers/UsersController/UserController.js
+++ src/controllers/UsersController/UserController.js
@@ -7,32 +7,6 @@ const CheckToken = require('../../Util/AuthJWT');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const HashPassword = require('../../Util/HashBcrypt');
-const GetUserName = require('../../Util/CreateUserName');
-
-router.patch('/:restaurantId/admin/user/:userId', CheckToken, async(req, res) => {
-  const { name, lastName, dateBirth, permissions } = req.body;
-  const { restaurantId, userId } = req.params;
-  const userName = GetUserName(name, dateBirth);
-  const userExists = await Users.findOne({ where: { id: userId } });
-
-  if (!userExists) {
-    res.status(401).json({ message: 'O usuário não existe' });
-  }
-
-  try {
-    Users.update({
-      name,
-      lastName,
-      dateBirth,
-      permissions,
-      userName,
-      restaurantId
-    }, { where: { id: userId } });
-    res.status(200).json({ message: 'Cadastro atualizado com sucesso!' });
-  } catch (err) {
-    res.send(400).json({ message: err });
-  }
-});
 
 router.patch('/:restaurantId/admin/user/:userId/pass', CheckToken, async(req, res) => {
   const { password, repeatpassword } = req.body;

2204876d50d5ba45d1f0c033be826272377a2d6b
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-03-22 18:16:18 -0300

    fix: refactor route for search snacks in database

--- src/controllers/SnacksController/SnacksController.js
+++ src/controllers/SnacksController/SnacksController.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const Snack = require('../../models/Snacks');
 const CheckToken = require('../../Util/AuthJWT');
 
-router.get('/additional', CheckToken, (req, res) => {
-  Snack.findall();
-  console.log('teste tabela');
+router.get('/:restaurantId/snacks', CheckToken, (req, res) => {
+  const { restaurantId } = req.params;
+
+  Snack.findall({ where: { restaurantId } })
+    .then((additionals) => {
+      res.status(200).json({ additionals });
+    })
+    .catch((err) => {
+      res.send(400).json({ message: err });
+    });
 });
 
 module.exports = router;

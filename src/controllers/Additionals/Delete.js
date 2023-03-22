5ec490a4261b15fef1ee89974119fe5304ff3142
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-03-22 18:47:44 -0300

    refactor

--- src/controllers/Additionals/Delete.js
+++ src/controllers/Additionals/Delete.js
@@ -3,20 +3,16 @@ const router = express.Router();
 const Additional = require('../../models/Additional');
 const CheckToken = require('../../Util/AuthJWT');
 
-router.delete('/:restaurantId/additional/:id/delete', CheckToken, async(req, res) => {
+router.delete('/:restaurantId/additional/:id/delete', CheckToken, (req, res) => {
   const { id } = req.params;
 
-  const itemExists = Additional.findOne({ where: { id } });
-  if (!itemExists) {
-    return res.status(401).json({ message: 'O item não esta cadastrado!' });
-  }
-
-  try {
-    Additional.destroy({ where: { id } });
-    res.status(200).json({ message: 'Item deletado com sucesso!' });
-  } catch (err) {
-    res.send(400).json({ message: err });
-  }
+  Additional.destroy({ where: { id } })
+    .then(() => {
+      res.status(200).json({ message: 'Item deletado com sucesso!' });
+    })
+    .catch((err) => {
+      res.send(400).json({ message: err });
+    });
 }
 );
 

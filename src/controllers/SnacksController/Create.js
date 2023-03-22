40a6ee020bb5828f1630367383e3b8ee585863c9
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-03-22 18:28:00 -0300

    feat: created route to inserting snacks in database

---
+++ src/controllers/SnacksController/Create.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const Snack = require('../../models/Snacks');
+const CheckToken = require('../../Util/AuthJWT');
+
+router.get('/:restaurantId/snacks', CheckToken, async(req, res) => {
+  const { restaurantId } = req.params;
+  const { type, size, flavor, description, price, categorie } = req.body;
+
+  if (!type) {
+    return res.status(422).json({ message: 'O tipo é obrigatório!' });
+  }
+
+  if (!size) {
+    return res.status(422).json({ message: 'O tamanho é obrigatório!' });
+  }
+
+  if (!flavor) {
+    return res.status(422).json({ message: 'O sabor é obrigatório!' });
+  }
+
+  if (!description) {
+    return res.status(422).json({ message: 'A descrição é obrigatória!' });
+  }
+
+  if (!price) {
+    return res.status(422).json({ message: 'O preço é obrigatório!' });
+  }
+
+  if (!categorie) {
+    return res.status(422).json({ message: 'A categoria é obrigatória!' });
+  }
+
+  const snackExists = await Snack.findOne({ where: { restaurantId } });
+  if (snackExists) {
+    return res.status(401).json({ message: 'O item ja esta cadastrado!' });
+  }
+});
+
+module.exports = router;

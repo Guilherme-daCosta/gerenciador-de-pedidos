a46d744f78dbedb10cf2776878be4f3a2c5055e5
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-03-22 18:45:28 -0300

    feat: created route to deleting snacks in database

---
+++ src/controllers/SnacksController/Delete.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const Snack = require('../../models/Snacks');
+const CheckToken = require('../../Util/AuthJWT');
+
+router.delete('/:restaurantId/snacks/:id', CheckToken, (req, res) => {
+  const { id } = req.params;
+
+  Snack.destroy({ where: { id } })
+    .then(() => {
+      res.status(200).json({ message: 'Item deletado com sucesso!' });
+    })
+    .catch((err) => {
+      res.send(400).json({ message: err });
+    });
+});
+
+module.exports = router;

4857239dbbd0f66ba382b4c695a0c04dff45d5f5
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-04-21 15:38:25 -0300

    feat: changed to postgres database

--- src/models/database.js
+++ src/models/database.js
@@ -1,14 +1,8 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
-const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASS;
 
-const connection = new Sequelize('order_manager', user, password, {
-  host,
-  dialect: 'mysql',
-  timezone: '-03:00',
-  port: 3306
-});
+const connection = new Sequelize(`postgres://${user}:${password}@lallah.db.elephantsql.com/${user}`);
 
 module.exports = connection;
